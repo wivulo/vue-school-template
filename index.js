b3d50422b88ec4e69aa95078e51508a672f32d79
wivulo <walterivulo@gmail.com>
2024-03-02 19:06:37 +0100

    create account completed

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import Vue from "./node_modules/vue/dist/vue.esm.browser.js";
+import { emailRegex } from "./config.js";
+import { passwordRegex } from "./config.js";
 
 const DropdownComponent = {
     template: "#dropdown-template",
@@ -11,6 +13,7 @@ const DropdownComponent = {
 }
 
 const HeaderComponent = {
+    props: ["user"],
     template: "#header-template",
     data() {
         return {
@@ -39,20 +42,90 @@ const HeaderComponent = {
     }
 }
 
+const FormComponent = {
+    template: "#form-template",
+    data() {
+        return {
+            name: "",
+            email: "",
+            password: "",
+            isCreateAccount: false,
+            errorMessage: ""
+        };
+    },
+    computed: {
+        isButtonDisabled() {
+            return !this.name || !this.email || !this.password;
+        }
+    },
+    methods: {
+        clearInputFields() {
+            this.name = "";
+            this.email = "";
+            this.password = "";
+        },
+        handlerError(message) {
+            this.errorMessage = message;
+            setTimeout(() => {
+                this.errorMessage = "";
+            }, 3000);
+        },
+        async submitForm() {
+            this.isCreateAccount = true;
+            //simulate server request
+            await  new Promise((resolve, reject) => setTimeout(() => resolve(true), 2000)) // sleep 2 sec
+
+            if(!emailRegex.test(this.email)) {
+                this.isCreateAccount = false;
+                this.handlerError("Invalid email");
+                return;
+            }
+
+            if(!passwordRegex.test(this.password)) {
+                this.isCreateAccount = false;
+                this.handlerError("Password must be at least 4 digits");
+                return;
+            }
+            
+            this.$emit("on-create-account", {
+                name: this.name,
+                email: this.email,
+                password: this.password
+            });
+            this.clearInputFields();
+            this.isCreateAccount = false;
+        }
+    },
+}
+
+
 const mainComponent = {
     template: "#main-template",
     data() {
         return {
             message: "Main component"
         };
+    },
+    components: {
+        "app-form": FormComponent
+    },
+    methods: {
+        handleCreateAccount(payload) {
+            this.$emit("on-create-account-tunnel", payload);
+        }
     }
-
 }
 
 new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue.js!',
+        user: {}
+    },
+    methods: {
+        handleCreateAccount(payload) {
+            this.user = payload;
+        }
     },
     components: {
         'app-header': HeaderComponent,
